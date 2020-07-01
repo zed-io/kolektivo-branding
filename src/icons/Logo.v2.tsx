2b578a86e8df633576656719701b99bbfb0d364c
Jean Regisser <jean.regisser@gmail.com>
2020-07-01 17:52:15 +0200

    Fix TypeScript error

--- src/icons/Logo.v2.tsx
+++ src/icons/Logo.v2.tsx
@@ -14,6 +14,8 @@ interface Props {
 export default function Logo({ height = 32, type = LogoTypes.COLOR }: Props) {
   return (
     <Svg width={height} height={height} viewBox="0 0 32 32" fill="none">
+      {/* 
+      // @ts-ignore */}
       <G filter="url(#prefix__filter0_d)">
         <Path
           d="M18.39 26.77c1.072-8.36 5.002-13.12 10.943-17.434l-3.037-4.003c-3.886 2.98-8.13 7.205-10.095 13.032-1.608-4.76-4.958-8.94-10.363-13.032L2.667 9.425C9.41 14.228 12.807 19.61 13.744 26.77h4.646z"
