a81b5aedb3bc79959022d421a5d3bbfb55dba4eb
Nitya Subramanian <nityas.subramanian@gmail.com>
2020-09-01 18:14:20 -0700

    Update brandingConfig.ts

--- src/brandingConfig.ts
+++ src/brandingConfig.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = 'Valora'
 export const CELO_FAUCET_LINK = 'https://celo.org/app'
 export const CELO_TERMS_LINK = 'https://celo.org/terms'
-export const TOS_LINK = 'https://celo.org/user-agreement'
+export const TOS_LINK = 'https://valoraapp.com/terms'
 export const FAQ_LINK = 'https://valoraapp.com/faq'
 export const FORUM_LINK = 'https://forum.celo.org/c/support'
 export const CELO_SUPPORT_EMAIL_ADDRESS = 'support@valoraapp.com'
