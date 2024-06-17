7cc2fff8b6df4ddf8d1c266f342657c619b5c238
Matthew Abraham <14793093+shottah@users.noreply.github.com>
2024-06-17 15:40:24 -0400

    Update brandingConfig.ts

--- src/brandingConfig.ts
+++ src/brandingConfig.ts
@@ -17,3 +17,5 @@ export const TRANSACTION_FEES_LEARN_MORE =
   'https://valora.xyz/support/transaction-fees-learn-more'
 export const INVITE_REWARDS_NFTS_LEARN_MORE = 'https://valora.xyz/support/invite-rewards-nfts-learn-more'
 export const INVITE_REWARDS_STABLETOKEN_LEARN_MORE = 'https://valora.xyz/support/invite-rewards-stabletoken-learn-more'
+export const EARN_STABLECOINS_LEARN_MORE = 'https://valoraapp.com/support/earn-on-your-stablecoins'
+
